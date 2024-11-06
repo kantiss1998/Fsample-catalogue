f8cb7c8dcd4c1106eb27be15390815eb1a3c7308
kantiss1998 <itkantiss@gmail.com>
2024-11-06 15:56:01 +0700

    search

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import Header from "../components/Header";
 import ProductsSubName from "../components/ProductsSubName";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts } from "../api/productSlice";
-import Footer from "../components/Footer";
 import Container from "../components/layout/Container";
+import product from "../data/ExApi.json"
 
 const SearchPage = () => {
   const uniqueProducts = [];
@@ -14,23 +11,17 @@ const SearchPage = () => {
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get("q");
 
-  const dispatch = useDispatch();
-  const { products, status, error } = useSelector((state) => state.products);
+  console.log(query);
 
-    products && products.forEach((data) => {
-        data.Products.forEach((data2) => {
+
+    product && product.forEach((data) => {
+        data.Product.forEach((data2) => {
             if (data2.name.toLowerCase() == query.toLocaleLowerCase() || data2.subName.toLowerCase() == query.toLocaleLowerCase()) {
                 uniqueProducts.push(data2)
             }
         })
     })
 
-    
-  useEffect(() => {
-    if (status === "idle") {
-      dispatch(fetchProducts());
-    }
-  }, [status, dispatch]);
   return (
     <div className="bg-gray ">
       <MainLayout pageTitle="Ansania Hijab" className="flex flex-col gap-10">
